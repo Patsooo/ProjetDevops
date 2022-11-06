0135a438bff8cd0f0799530c7f7b116c13649e5e
youssefmansour12 <117481312+youssefmansour12@users.noreply.github.com>
2022-11-06 18:38:19 +0100

    Update FactureRestController.java

--- src/main/java/com/esprit/examen/controllers/FactureRestController.java
+++ src/main/java/com/esprit/examen/controllers/FactureRestController.java
@@ -2,83 +2,82 @@ package com.esprit.examen.controllers;
 
 import java.util.Date;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
-import com.esprit.examen.entities.Produit;
-import com.esprit.examen.services.IProduitService;
+import com.esprit.examen.entities.Facture;
+import com.esprit.examen.services.IFactureService;
 
 import io.swagger.annotations.Api;
 
 
 @RestController
+@Api(tags = "Gestion des factures")
+@RequestMapping("/facture")
 @CrossOrigin("*")
-@Api(tags = "Gestion des produits")
-@RequestMapping("/produit")
-public class ProduitRestController {
+public class FactureRestController {
 
-	@Autowired
-	IProduitService produitService;
+    @Autowired
+    IFactureService factureService;
 
-	// http://localhost:8089/SpringMVC/produit/retrieve-all-produits
-	@GetMapping("/retrieve-all-produits")
-	@ResponseBody
-	public List<Produit> getProduits() {
-		List<Produit> list = produitService.retrieveAllProduits();
-		return list;
-	}
+    // http://localhost:8089/SpringMVC/facture/retrieve-all-factures
+    @GetMapping("/retrieve-all-factures")
+    @ResponseBody
+    public List<Facture> getFactures() {
+        List<Facture> list = factureService.retrieveAllFactures();
+        return list;
+    }
 
-	// http://localhost:8089/SpringMVC/produit/retrieve-produit/8
-	@GetMapping("/retrieve-produit/{produit-id}")
-	@ResponseBody
-	public Produit retrieveRayon(@PathVariable("produit-id") Long produitId) {
-		return produitService.retrieveProduit(produitId);
-	}
+    // http://localhost:8089/SpringMVC/facture/retrieve-facture/8
+    @GetMapping("/retrieve-facture/{facture-id}")
+    @ResponseBody
+    public Facture retrieveFacture(@PathVariable("facture-id") Long factureId) {
+        return factureService.retrieveFacture(factureId);
+    }
 
-	/* Ajouter en produit tout en lui affectant la catégorie produit et le stock associés */
-	// http://localhost:8089/SpringMVC/produit/add-produit/{idCategorieProduit}/{idStock}
-	@PostMapping("/add-produit")
-	@ResponseBody
-	public Produit addProduit(@RequestBody Produit p) {
-		Produit produit = produitService.addProduit(p);
-		return produit;
-	}
+    // http://localhost:8089/SpringMVC/facture/add-facture/{fournisseur-id}
+    @PostMapping("/add-facture")
+    @ResponseBody
+    public Facture addFacture(@RequestBody Facture f) {
+        Facture facture = factureService.addFacture(f);
+        return facture;
+    }
 
+    /*
+     * une facture peut etre annulé si elle a été saisie par erreur Pour ce
+     * faire, il suffit de mettre le champs active à false
+     */
+    // http://localhost:8089/SpringMVC/facture/cancel-facture/{facture-id}
+    @PutMapping("/cancel-facture/{facture-id}")
+    @ResponseBody
+    public void cancelFacture(@PathVariable("facture-id") Long factureId) {
+        factureService.cancelFacture(factureId);
+    }
 
-	@DeleteMapping("/remove-produit/{produit-id}")
-	@ResponseBody
-	public void removeProduit(@PathVariable("produit-id") Long produitId) {
-		produitService.deleteProduit(produitId);
-	}
+    // http://localhost:8089/SpringMVC/facture/getFactureByFournisseur/{fournisseur-id}
+    @GetMapping("/getFactureByFournisseur/{fournisseur-id}")
+    @ResponseBody
+    public List<Facture> getFactureByFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
+        return factureService.getFacturesByFournisseur(fournisseurId);
+    }
 
+    // http://localhost:8089/SpringMVC/facture/assignOperateurToFacture/1/1
+    @PutMapping(value = "/assignOperateurToFacture/{idOperateur}/{idFacture}")
+    public void assignOperateurToFacture(@PathVariable("idOperateur") Long idOperateur, @PathVariable("idFacture") Long idFacture) {
+        factureService.assignOperateurToFacture(idOperateur, idFacture);
+    }
 
-	@PutMapping("/modify-produit")
-	@ResponseBody
-	public Produit modifyProduit(@RequestBody Produit p) {
-		return produitService.updateProduit(p);
-	}
-
-	/*
-	 * Si le responsable magasin souhaite modifier le stock du produit il peut
-	 * le faire en l'affectant au stock en question
-	 */
-
-	@PutMapping(value = "/assignProduitToStock/{idProduit}/{idStock}")
-	public void assignProduitToStock(@PathVariable("idProduit") Long idProduit, @PathVariable("idStock") Long idStock) {
-		produitService.assignProduitToStock(idProduit, idStock);
-	}
-
-	/*
-	 * Revenu Brut d'un produit (qte * prix unitaire de toutes les lignes du
-	 * detailFacture du produit envoyé en paramètre )
-	 */
-
-/*	@GetMapping(value = "/getRevenuBrutProduit/{idProduit}/{startDate}/{endDate}")
-	public float getRevenuBrutProduit(@PathVariable("idProduit") Long idProduit,
-			@PathVariable(name = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
-			@PathVariable(name = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
-
-		return produitService.getRevenuBrutProduit(idProduit, startDate, endDate);
-	}*/
+    // http://localhost:8089/SpringMVC/facture/pourcentageRecouvrement/{startDate}/{endDate}
+    @GetMapping(value = "/pourcentageRecouvrement/{startDate}/{endDate}")
+    public float pourcentageRecouvrement(
+            @PathVariable(name = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
+            @PathVariable(name = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
+        try {
+            return factureService.pourcentageRecouvrement(startDate, endDate);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
 
 }
