6477ea17c439c20d2f2fd2d268900ba27e9edc93
youssefmansour12 <117481312+youssefmansour12@users.noreply.github.com>
2022-11-13 20:16:09 +0100

    Update ProduitServiceImpl.java

--- src/main/java/com/esprit/examen/services/ProduitServiceImpl.java
+++ src/main/java/com/esprit/examen/services/ProduitServiceImpl.java
@@ -1,11 +1,11 @@
 package com.esprit.examen.services;
 
-import java.util.Date;
+
 import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.esprit.examen.entities.CategorieProduit;
+
 import com.esprit.examen.entities.Produit;
 import com.esprit.examen.entities.Stock;
 import com.esprit.examen.repositories.CategorieProduitRepository;
@@ -13,6 +13,7 @@ import com.esprit.examen.repositories.ProduitRepository;
 import com.esprit.examen.repositories.StockRepository;
 import lombok.extern.slf4j.Slf4j;
 
+import static org.mockito.ArgumentMatchers.notNull;
 @Service
 @Slf4j
 public class ProduitServiceImpl implements IProduitService {
@@ -60,11 +61,13 @@ public class ProduitServiceImpl implements IProduitService {
 
 	@Override
 	public void assignProduitToStock(Long idProduit, Long idStock) {
-		Produit produit = produitRepository.findById(idProduit).orElse(null);
-		Stock stock = stockRepository.findById(idStock).orElse(null);
-		produit.setStock(stock);
-		produitRepository.save(produit);
-
+		Produit produit = null;
+		if (idProduit == notNull()) {
+			produit = produitRepository.findById(idProduit).orElseThrow(NullPointerException::new);
+			Stock stock = stockRepository.findById(idStock).orElseThrow(NullPointerException::new);
+			produit.setStock(stock);
+			produitRepository.save(produit);
+		}
 	}
 
 
