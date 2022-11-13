72f81480f2311cf2db9d622f50ccd6726c581f9a
youssefmansour12 <117481312+youssefmansour12@users.noreply.github.com>
2022-11-13 20:38:09 +0100

    Update FournisseurServiceImpl.java

--- src/main/java/com/esprit/examen/services/FournisseurServiceImpl.java
+++ src/main/java/com/esprit/examen/services/FournisseurServiceImpl.java
@@ -33,7 +33,7 @@ public class FournisseurServiceImpl implements IFournisseurService {
 
 	@Override
 	public List<Fournisseur> retrieveAllFournisseurs() {
-		List<Fournisseur> fournisseurs = (List<Fournisseur>) fournisseurRepository.findAll();
+		List<Fournisseur> fournisseurs =  fournisseurRepository.findAll();
 		for (Fournisseur fournisseur : fournisseurs) {
 			log.info(" fournisseur : " + fournisseur);
 		}
@@ -43,8 +43,8 @@ public class FournisseurServiceImpl implements IFournisseurService {
 
 	public Fournisseur addFournisseur(Fournisseur f ) {
 		DetailFournisseur df= new DetailFournisseur();
-		df.setDateDebutCollaboration(new Date()); 
-		
+		df.setDateDebutCollaboration(new Date());
+
 		f.setDetailFournisseur(df);	
 		fournisseurRepository.save(f);
 		return f;
