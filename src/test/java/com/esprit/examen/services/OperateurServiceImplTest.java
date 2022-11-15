a700c7cbdf9de470c503be207da6c7e731e9162f
gharbi haithem <73082597+haythemGHA@users.noreply.github.com>
2022-11-15 03:00:16 +0100

    Update OperateurServiceImplTest.java

--- src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
@@ -17,58 +17,7 @@ import com.esprit.examen.repositories.OperateurRepository;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OperateurServiceImplTest {
-    /*@Autowired
-       IOperateurService operateurService;
-       private static final Logger l = LogManager.getLogger(OperateurServiceImpl.class);
-
-       @Test
-       public void testRetrieveAllOperateur() throws ParseException {
-
-         List<Operateur> operateur = operateurService.retrieveAllOperateurs();
-         Operateur op = new Operateur(null, "testnom","testprenom","testpassword", null);
-         Operateur savedOperateur = operateurService.addOperateur(op);
-         l.info(" Operateur : " + op);
-
-         assertNotNull(op.getIdOperateur());
-
-         operateurService.retrieveOperateur(op.getIdOperateur());
-
-         List<Operateur> Operateurs = operateurService.retrieveAllOperateurs();
-         l.info("Les  Operateurs : " + Operateurs);
-       }
-       
-       
-       @Test
-       public void testDeleteOperateur() throws ParseException {
-
-    	    List<Operateur> Operateurs = operateurService.retrieveAllOperateurs();
-    	   int expected = Operateurs.size();
-    	   Long idFour=(long) 2;
-    	   assertEquals(expected + 1, operateurService.retrieveAllOperateurs().size());
-    	   System.out.print("size "+Operateurs.size());
-    	   l.info(" count" + Operateurs.size());
-    	   operateurService.deleteOperateur(1L);
-    	   l.info("size2 "+operateurService.retrieveAllOperateurs().size());
-
-       }
-   
-   
-       @Test
-       public void testAddOperateur() throws ParseException{
-    	   
-    	   List<Operateur> Operateurs = operateurService.retrieveAllOperateurs();
-    	   Operateur op = new Operateur(null, "testnom2","testprenom2","testpassword2", null);
-    	   l.info("operateur \n "+op);
-
-    	   Operateur savedOperateur= operateurService.addOperateur(op);
-    	   l.info("size1 "+Operateurs.size());
-    	   System.out.print("size1 "+Operateurs.size());
-    	   operateurService.deleteOperateur(savedOperateur.getIdOperateur());
-    	   List<Operateur> Operateurs1 = operateurService.retrieveAllOperateurs();
-
-    	   l.info("size2 "+Operateurs1.size());
-       }
-       */
+    
 	@Autowired
     private OperateurServiceImpl operateurServiceImpl;
 	
