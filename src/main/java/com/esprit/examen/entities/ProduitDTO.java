3a86b7f1ee98fecb9dfb7d2ff660a1648ab73cf1
youssefmansour12 <117481312+youssefmansour12@users.noreply.github.com>
2022-11-15 17:51:00 +0100

    Delete ProduitDTO.java

--- src/main/java/com/esprit/examen/entities/ProduitDTO.java
+++
@@ -1,9 +0,0 @@
-package com.esprit.examen.dto;
-import lombok.Data;
-@Data
-public class ProduitDTO {
-	private Long idProduit;
-	private String codeProduit;
-	private String libelleProduit;
-	private float prix;
-}
