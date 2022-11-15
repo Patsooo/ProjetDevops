6d6d51c942e0c8bd89209621ffc154e983425209
youssefmansour12 <117481312+youssefmansour12@users.noreply.github.com>
2022-11-15 17:45:28 +0100

    Add files via upload

---
+++ src/main/java/com/esprit/examen/entities/ProduitDTO.java
@@ -0,0 +1,9 @@
+package com.esprit.examen.dto;
+import lombok.Data;
+@Data
+public class ProduitDTO {
+	private Long idProduit;
+	private String codeProduit;
+	private String libelleProduit;
+	private float prix;
+}
