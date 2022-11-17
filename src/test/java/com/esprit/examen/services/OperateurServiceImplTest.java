2c3bb4938de1b0241bbc3cea308d6ec44da5fd6b
gharbi haithem <73082597+haythemGHA@users.noreply.github.com>
2022-11-17 23:10:29 +0100

    Update OperateurServiceImplTest.java

--- src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
@@ -2,40 +2,62 @@ package com.esprit.examen.services;
 
 
 import java.util.List;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import com.esprit.examen.entities.Operateur;
-import com.esprit.examen.repositories.OperateurRepository;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OperateurServiceImplTest {
-    
 	@Autowired
-    private OperateurServiceImpl operateurServiceImpl;
-	
-	@Autowired
-    private OperateurRepository operateurRepository;
-	
-	
-       Operateur operateur = new Operateur(null, "haythem", "gharbi", "gha1920", null);
-       @Test
-       @Order(1)
-       void testaddOperateur() {
-       	Operateur operateurAdded =  operateurServiceImpl.addOperateur(operateur);
-           Assertions.assertEquals(operateurAdded.getIdOperateur(), operateurAdded.getIdOperateur());
-       }
-       @Test
-       @Order(2)
-       void testRetrieveAllOperateurs() {
-           List<Operateur> listOperateurs = operateurServiceImpl.retrieveAllOperateurs();
-           Assertions.assertEquals(listOperateurs.size(), listOperateurs.size());
-       }
+	IOperateurService operateurService;
+
+	@Test
+	@Order(4)
+	public void testRetrieveAllOperateurs() {
+		List<Operateur> listOperateur = operateurService.retrieveAllOperateurs();
+		Assertions.assertEquals(0, listOperateur.size());
+	}
+	@Test
+	@Order(1)
+	public void testAddOperateur() {
+		List<Operateur> operateurs = operateurService.retrieveAllOperateurs();
+		int expected=operateurs.size();
+		Operateur o = new Operateur(null,"hai","gha","haigha",null);
+		Operateur savedOperateur= operateurService.addOperateur(o);
+
+		assertEquals(expected+1, operateurService.retrieveAllOperateurs().size());
+		assertNotNull(savedOperateur.getIdOperateur());
+		operateurService.deleteOperateur(savedOperateur.getIdOperateur());
+
+	}
+
+	@Test
+	@Order(3)
+	public void testAddOperateurOptimized() {
+
+		Operateur o = new Operateur(null,"haithem","gharbi","haigha",null);
+		Operateur savedOperateur= operateurService.addOperateur(o);
+		assertNotNull(savedOperateur.getIdOperateur());
+		assertSame("haithem", savedOperateur.getNom());
+		assertTrue(savedOperateur.getPassword()=="haigha");
+		operateurService.deleteOperateur(savedOperateur.getIdOperateur());
+
+	}
+
+	@Test
+	@Order(2)
+	public void testDeleteOperateur() {
+		Operateur o = new Operateur(null,"haithem","gharbi","haigha",null);
+		Operateur savedOperateur= operateurService.addOperateur(o);
+		operateurService.deleteOperateur(savedOperateur.getIdOperateur());
+		assertNull(operateurService.retrieveOperateur(savedOperateur.getIdOperateur()));
+	}
 }
