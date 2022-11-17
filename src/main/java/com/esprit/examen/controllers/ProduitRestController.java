6297111970e2ad7a73a95680e811d778bbca7dee
youssefmansour12 <117481312+youssefmansour12@users.noreply.github.com>
2022-11-17 21:04:21 +0100

    Update ProduitRestController.java

--- src/main/java/com/esprit/examen/controllers/ProduitRestController.java
+++ src/main/java/com/esprit/examen/controllers/ProduitRestController.java
@@ -1,15 +1,11 @@
 package com.esprit.examen.controllers;
 
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.esprit.examen.entities.Produit;
 import com.esprit.examen.services.IProduitService;
-
 import io.swagger.annotations.Api;
-
-
 @RestController
 /**@CrossOrigin("*")**/
 @Api(tags = "Gestion des produits")
@@ -26,29 +22,21 @@ public class ProduitRestController {
 		return produitService.retrieveAllProduits();
 	}
 
-
 	@GetMapping("/retrieve-produit/{produit-id}")
 	@ResponseBody
 	public Produit retrieveRayon(@PathVariable("produit-id") Long produitId) {
 		return produitService.retrieveProduit(produitId);
 	}
-
-
-
 	@PostMapping("/add-produit")
 	@ResponseBody
 	public Produit addProduit(@RequestBody Produit p) {
 		return produitService.addProduit(p);
 	}
-
-
 	@DeleteMapping("/remove-produit/{produit-id}")
 	@ResponseBody
 	public void removeProduit(@PathVariable("produit-id") Long produitId) {
 		produitService.deleteProduit(produitId);
 	}
-
-
 	@PutMapping("/modify-produit")
 	@ResponseBody
 	public Produit modifyProduit(@RequestBody Produit p) {
@@ -58,12 +46,5 @@ public class ProduitRestController {
 	public void assignProduitToStock(@PathVariable("idProduit") Long idProduit, @PathVariable("idStock") Long idStock) {
 		produitService.assignProduitToStock(idProduit, idStock);
 	}
-
-
-	
-
-
-
-
 
 }
