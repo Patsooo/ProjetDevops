f50891a18770a998afe472bb3921fedf5fb2ce81
gharbi haithem <73082597+haythemGHA@users.noreply.github.com>
2022-11-18 01:26:15 +0100

    Update FactureRestController.java

--- src/main/java/com/esprit/examen/controllers/FactureRestController.java
+++ src/main/java/com/esprit/examen/controllers/FactureRestController.java
@@ -25,8 +25,8 @@ public class FactureRestController {
     @GetMapping("/retrieve-all-factures")
     @ResponseBody
     public List<Facture> getFactures() {
-        List<Facture> list = factureService.retrieveAllFactures();
-        return list;
+        
+        return factureService.retrieveAllFactures();
     }
 
     // http://localhost:8089/SpringMVC/facture/retrieve-facture/8
